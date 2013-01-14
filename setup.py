f3f03d6bd33801a350e043d17bcaa7a5d8cf463e
James Sutterfield <james.sutterfield@gmail.com>
2013-01-13 17:47:46 -0800

    fixed merge error in setup.py

--- setup.py
+++ setup.py
@@ -8,20 +8,11 @@ setup(
               'climbs',
               'users'],
     install_requires=[
-<<<<<<< HEAD
-        'Django==1.4.2',
-        'South==0.7.6',
-        'django-autoslug==1.6.1',
-        'PIL==1.1.7'
-        'requests==1.0.4'
-        'dropbox==1.5.1']
-=======
         'Django == 1.4.2',
         'South == 0.7.6',
         'django-autoslug == 1.6.1',
         'PIL == 1.1.7',
         'requests == 1.0.4']
->>>>>>> 8fe078d02da77a403085ea66667366ee1918d1ea
 )
 
 
