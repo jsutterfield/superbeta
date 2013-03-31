36719fcea6bb7aefc62f939be59cecfbb5bca759
James Sutterfield <james.sutterfield@gmail.com>
2013-03-31 13:06:32 -0700

    refactored models

--- climbs/models.py
+++ climbs/models.py
@@ -2,28 +2,100 @@ from django.db import models
 from autoslug import AutoSlugField
 from django.contrib.localflavor.us.models import USStateField
 
-STAR_RATING = (
-    # Look into having the first value be nums, not strings.
-    ('1', "One Star"),
-    ('2', "Two Star"),
-    ('3', "Three Star"),
-    ('4', "Four Star"),
-)
-
-    
-class Route(models.Model):
-
-    name = models.CharField(max_length=100)
-    difficulty = models.CharField(max_length=15)
-    description = models.TextField(blank=True)
-    quality_rating = models.CharField(max_length=1, choices=STAR_RATING, blank=True)
-    height = models.IntegerField(null=True, blank=True)
-    weather = models.TextField(blank=True)
-    history = models.TextField(blank=True)
-    area = models.ForeignKey('Area')
+class Area(models.Model):
+    AREA_CHOICES = (
+        ("RE", "Region"),
+        ("AR", "AREA"),
+        ("WA", "Wall")
+    )
+    APPROACH_CHOICES = (
+        ("Easy", "Flip Flops"),
+        ("Intermediate", "Sneakers"),
+        ("Difficult", "Boots")
+    )
+    PARKING_TYPE = (
+        ("L", "Lot"),
+        ("S", "Street"),
+        ("G", "Garage")
+    )
+    area_type = models.CharField(max_length=2, choices=AREA_CHOICES)
+    area_parent = models.ManyToManyField("self")
+    state = USStateField()
+    city = models.CharField(max_length=20)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    street_address = models.CharField(max_length=40, blank=True)
+    zipcode = models.SmallIntegerField(null=True, blank=True)
+    bouldering = models.BooleanField(default=False, blank=True)
+    top_rope = models.BooleanField(default=False, blank=True)
+    sport = models.BooleanField(default=False, blank=True)
+    trad = models.BooleanField(default=False, blank=True)
+    approach_difficulty = models.CharField(max_length=12, choices=APPROACH_CHOICES, blank=True)
+    approach_time = models.SmallIntegerField(null=True, blank=True)
+    approach_distance = models.SmallIntegerField(null=True, blank=True)
+    approach_description = models.CharField(blank=True)
+    trailhead_longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    trailhead_latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    parking_type = models.CharField(max_length=1, choices=PARKING_TYPE, blank=True)
+    parking_longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    parking_latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    parking_description = models.CharField(blank=True)
+    height = models.SmallIntegerField(null=True, blank=True)
+    short_description = models.CharField(max_length=120, blank=True)
+    about = models.CharField(blank=True)
+    description = models.CharField(blank=True)
+    rock_type = models.CharField(blank=True)
+    weather_description = models.CharField(blank=True)
+    climbing_season = models.CharField(blank=True)
+    hazard_information = models.CharField(blank=True)
+    misc_information = models.CharField(blank=True)
+    pet_friendly = models.BooleanField(default=False)
+    nearest_emergency = models.CharField(blank=True)
+    nearest_emergency_address = models.CharField(max_length=40, blank=True)
+    nearest_emergency_phone = models.SmallIntegerField(null=True, blank=True)
+    nearest_emergttency_two_address = models.CharField(max_length=40, blank=True)
     slug = AutoSlugField(populate_from='name', unique_with='area__name')
     featured = models.BooleanField(default=False)
 
+    
+class Route(models.Model):
+    ROUTE_QUALITY = (
+        # Look into having the first value be nums, not strings.
+        ('1', "One Star"),
+        ('2', "Two Star"),
+        ('3', "Three Star"),
+        ('4', "Four Star"),
+        ('5', "Five Star")
+    )
+    ANGLE_CHOICES = (
+        ('S', 'Slab'),
+        ('N', '90 Degrees'),
+        ('O', 'Overhang'),
+        ('R', 'Roof')
+    )
+    name = models.CharField(max_length=100)
+    parent = ForeignKey('Area')
+    grade = models.CharField(max_length=6)
+    quality = models.CharField(max_length=1, choices=ROUTE_QUALITY, blank=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    description = models.TextField(blank=True)
+    quality_rating = models.CharField(max_length=1, choices=STAR_RATING, blank=True)
+    height = models.SmallIntegerField(null=True, blank=True)
+    angle = models.CharField(max_length=1, choices=ANGLE_CHOICES, blank=True)
+    features = models.CharField(blank=True)
+    short_description = models.CharField(max_length=120, blank=True)
+    about = models.CharField(blank=True)
+    location = models.CharField(blank=True)
+    route_start = models.CharField(blank=True)
+    route_descent = models.CharField(blank=True)
+    landing_information = models.CharField(blank=True)
+    hazard_information = models.CharField(blank=True)
+    aspect = models.CharField(blank=True)
+    variations = models.CharField(blank=True)
+    misc_information = models.CharField(blank=True)
+    first_ascent = models.CharField(blank=True)
+    source = models.CharField(blank=True)
 
     def __unicode__(self):
         return self.name
@@ -56,52 +128,14 @@ class RoutePhoto(models.Model):
         return "%s-%s" % (self.route, self.photo_type)
 
 
-class Area(models.Model):
-
-    APPROACH_CHOICES = (
-        ("Easy", "Flip Flops"),
-        ("Intermediate", "Sneakers"),
-        ("Difficult", "Boots")
-    )
-
-    name = models.CharField(max_length=100)
-    # Should we allow area to have the same name if in different States?
-    slug = AutoSlugField(populate_from='name', unique=True)
-    about = models.TextField(blank=True)
-    parking_desc = models.TextField(blank=True)
-    parking_gps_long = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
-    parking_gps_lat = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
-    approach_desc = models.TextField(blank=True)
-    approach_gps_long = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
-    approach_gps_lat = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
-    approach_rating = models.CharField(max_length=15, choices=APPROACH_CHOICES, blank=True)
-    approach_length = models.TextField(blank=True)
-    weather = models.TextField(blank=True)
-    misc = models.TextField(blank=True)
-    state = USStateField(blank=True, null=True)
-    closest_city = models.CharField(blank=True, null=True, max_length=30)
-    zipcode = models.CharField(blank=True, null=True, max_length=10)    
-
-    def __unicode__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return "/areas/%s/" % self.slug
-
-
 class AreaPhoto(models.Model):
-
     PHOTO_CHOICES = (
         ("G", "General"),
         ("A", "Approach"),
     )
-    
-    # Figure out a better naming scheme for where the photos
-    # are uploaded to
+    area = models.ForeignKey('Area')
     photo = models.ImageField(upload_to="photos")
     photo_type = models.CharField(max_length=1, choices=PHOTO_CHOICES)
-    area = models.ForeignKey('Area')
-    # Look into changing this field - chartype doesn't seem right
     quality_rating = models.CharField(max_length=1, choices=STAR_RATING, blank=True)
 
     def __unicode__(self):
