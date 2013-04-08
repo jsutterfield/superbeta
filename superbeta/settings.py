37b7098b16eab6493105c4800d95a21f7e592d52
James Sutterfield <james.sutterfield@gmail.com>
2013-04-07 20:24:41 -0700

    removed users (for now)

--- superbeta/settings.py
+++ superbeta/settings.py
@@ -19,10 +19,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(PROJECT_ROOT, 'sqlite.db'),  # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'django_db',  # Or path to database file if using sqlite3.
+        'USER': 'django_login',                      # Not used with sqlite3.
+        'PASSWORD': 'lurkingfear415',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
--- superbeta/views.py
+++ superbeta/views.py
@@ -5,48 +5,50 @@ from climbs.models import Route, Area, RoutePhoto, AreaPhoto
 from random import choice
 import weather
 
-# def home(request):
-#     featured_route = None
-#     featured_photos = None
-#     area_photos = None
-#     try:
-#         featured_route = choice(Route.objects.filter(featured=True))
-#     except IndexError:
-#         pass
-#     if featured_route:
-#         featured_route.has_newline = False
-#         featured_photos = featured_route.routephoto_set.all()
-#         if featured_route.description.count('\n', 0, 420) > 0:
-#             if featured_route.description.count('\n', 0, 420) > 1:
-#                 featured_route.has_multiple_newlines = True
-#             else:
-#                 featured_route.has_newline = True
-#         area_photos = AreaPhoto.objects.filter(photo_type="G")
-#     return render(request, 'index.html', {'featured_route': featured_route,
-#                                           'featured_photos': featured_photos,
-#                                           'area_photos': area_photos}) 
-
-# def route(request, area_slug, route_slug):
-#     route = None
-#     photos = None
-#     try:
-#         route = Route.objects.get(slug=route_slug, area__slug=area_slug)
-#         photos = route.routephoto_set.all()
-#     except:
-#         raise Http404
-#     return render(request, 'route.html', {'route': route, 'photos': photos})
-
-# def area(request, area_slug):
-#     try:
-#         area = Area.objects.get(slug=area_slug)
-#     except Area.DoesNotExist:
-#         raise Http404
-#     photos = area.areaphoto_set.all()
-#     routes = area.route_set.all()
-#     classic_photos = RoutePhoto.objects.filter(route__area=area, photo_type="T") 
-#     weather_forecast = weather.get_forecast(city=area.closest_city, state=area.state)
-#     return render(request, 'area.html', {'area': area, 'photos': photos, 'classic_photos': classic_photos, 
-#                                          'weather': weather_forecast})
-
-def area(request):
+def home(request):
+    featured_route = None
+    featured_photos = None
+    area_photos = None
+    try:
+        featured_route = choice(Route.objects.filter(featured=True))
+    except IndexError:
+        pass
+    if featured_route:
+        featured_route.has_newline = False
+        featured_photos = featured_route.routephoto_set.all()
+        if featured_route.description.count('\n', 0, 420) > 0:
+            if featured_route.description.count('\n', 0, 420) > 1:
+                featured_route.has_multiple_newlines = True
+            else:
+                featured_route.has_newline = True
+        area_photos = AreaPhoto.objects.filter(photo_type="G")
+    return render(request, 'index.html', {'featured_route': featured_route,
+                                          'featured_photos': featured_photos,
+                                          'area_photos': area_photos}) 
+def region(request):
     return HttpResponse('success!')
+
+def area(request, area_slug):
+    try:
+        area = Area.objects.get(slug=area_slug)
+    except Area.DoesNotExist:
+        raise Http404
+    photos = area.areaphoto_set.all()
+    routes = area.route_set.all()
+    classic_photos = RoutePhoto.objects.filter(route__area=area, photo_type="T") 
+    weather_forecast = weather.get_forecast(city=area.closest_city, state=area.state)
+    return render(request, 'area.html', {'area': area, 'photos': photos, 'classic_photos': classic_photos, 
+                                         'weather': weather_forecast})
+
+def boulder(request):
+    return HttpResponse('success!')
+
+def route(request, area_slug, route_slug):
+    route = None
+    photos = None
+    try:
+        route = Route.objects.get(slug=route_slug, area__slug=area_slug)
+        photos = route.routephoto_set.all()
+    except:
+        raise Http404
+    return render(request, 'route.html', {'route': route, 'photos': photos})
