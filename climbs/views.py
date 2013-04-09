7ece67e51b17298232a6ba582b1445e5d6ab1028
James Sutterfield <james.sutterfield@gmail.com>
2013-04-08 22:58:01 -0700

    working views for all of the area and problem urls

--- climbs/views.py
+++ climbs/views.py
@@ -5,27 +5,41 @@ from climbs.models import Problem, Area, ProblemPhoto, AreaPhoto
 from random import choice
 from superbeta import weather
 
-def region(request, region_slug=None):
-    return HttpResponse('Welcome to regions!')
+def region(request, region_slug):
+    try:
+        region = Area.objects.get(slug=region_slug)
+    except Area.DoesNotExist:
+        raise Http404
+    return HttpResponse('Welcome to %s!' % region.name)
 
-def area(request, region_slug=None, area_slug=None):
-    return HttpResponse("Welcome to area!")
-    # try:
-    #     area = Area.objects.get(slug=area_slug)
-    # except Area.DoesNotExist:
-    #     raise Http404
+def area(request, region_slug, area_slug):
+    try:
+        area = Area.objects.filter(slug=area_slug, area_parent__slug=region_slug).get()
+    except Area.DoesNotExist:
+        raise Http404
     # photos = area.areaphoto_set.all()
     # problems = area.Problem_set.all()
     # classic_photos = ProblemPhoto.objects.filter(problem__area=area, photo_type="T") 
     # weather_forecast = weather.get_forecast(city=area.closest_city, state=area.state)
+    return HttpResponse("Welcome to %s" % area.name)
     # return render(request, 'area.html', {'area': area, 'photos': photos, 'classic_photos': classic_photos, 
     #                                      'weather': weather_forecast})
 
-def boulder(request, region_slug=None, area_slug=None, boulder_slug=None):
-    return HttpResponse('Welcome to boulder!')
+def boulder(request, region_slug, area_slug, boulder_slug):
+    try:
+        boulder = Area.objects.filter(slug=boulder_slug, area_parent__slug=area_slug,
+                                      area_parent__area_parent__slug=region_slug).get()
+    except Area.DoesNotExist:
+        raise Http404
+    return HttpResponse('Welcome to %s!' % boulder.name)
 
-def problem(request, region_slug=None, area_slug=None, boulder_slug=None, problem_slug=None):
-    return HttpResponse("Welcome to problem!")
+def problem(request, region_slug, area_slug, boulder_slug, problem_slug):
+    try:        
+        problem = Problem.objects.filter(slug=problem_slug, parent__slug=boulder_slug,
+                                      parent__area_parent__slug=area_slug).get()
+    except Area.DoesNotExist:
+        raise Http404
+    return HttpResponse('Welcome to %s!' % problem.name)
     # problem = None
     # photos = None
     # try:
